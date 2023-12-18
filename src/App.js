16e8ef4b750f8ba741b0e989c25f624eb51e7023
ADRIXUS <kbhautik6>
2023-12-18 15:23:55 +0530

    storybook setup

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import {index} from './index';
+
+console.warn(index(),"app file");
---
+++ src/theme.js
@@ -0,0 +1,42 @@
+export const theme={
+    primarycolor: '#ff4e54',
+    defaultcolor: '#4a4a68',
+    secondarycolor: '#f3f4f6',
+    dangercolor: '#f44336',
+    successcolor: '#4caf50',
+    warningcolor: '#ff9800',
+    activecolor: '#ffeded',
+    primaryhover: '#ff7176',
+    secondaryhover: '#e5e7eb',
+    dangerhover: '#d31609',
+    successhover: '#0d8611',
+    activehover: '#fbe6dc',
+    offwhite: '#f6f9fc',
+    white:'ffffff',
+    black:'#000000'
+};
+export const avatar={
+    small:{
+        width:'50px',
+        height:'50px'
+    },
+    medium:{
+        width:'60px',
+        height: '60px'
+    }
+    ,
+
+    large:{
+        width:'60px',
+        height:'80px'
+    },
+    default:{
+        width:'70px',
+        height:'70px'
+    }
+
+}
+
+export const topography={
+    backgroundcolor:'rgb(255, 237, 237)',
+}
