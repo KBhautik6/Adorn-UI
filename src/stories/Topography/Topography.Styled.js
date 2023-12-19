b4c4459310dcc20edd3390402b29632652d822b0
ADRIXUS <kbhautik6>
2023-12-19 21:36:20 +0530

    Complete all Typography

---
+++ src/stories/Topography/Topography.Styled.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+import { theme } from "../../theme";
+
+export const Styledheading = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Textcontent = styled.div`
+  color: ${({ type }) => theme?.[type]};
+
+  ${({ as }) =>
+    as &&
+    css`
+      line-height: 0.5;
+      font-weight: 600;
+      font-family: "Rubik", sans-serif;
+    `}
+`;
--- src/theme.js
+++ src/theme.js
@@ -1,42 +1,21 @@
 export const theme={
-    primarycolor: '#ff4e54',
-    defaultcolor: '#4a4a68',
-    secondarycolor: '#f3f4f6',
-    dangercolor: '#f44336',
-    successcolor: '#4caf50',
-    warningcolor: '#ff9800',
-    activecolor: '#ffeded',
-    primaryhover: '#ff7176',
-    secondaryhover: '#e5e7eb',
-    dangerhover: '#d31609',
-    successhover: '#0d8611',
-    activehover: '#fbe6dc',
-    offwhite: '#f6f9fc',
-    white:'ffffff',
-    black:'#000000'
+    primary: "#FF4E54",
+    secondary: "#FF8242",
+    success: "#07CB6E",
+    warning: "#F3B664",
+    danger:"#F3B664",
+    cardBorderColor: "#E49393",
+    cardTitleColor: "#060047",
+    cardTextColor: "#1640D6",
+    cardButtonBg: "#E90064",
+    cardButtonHoverBg: "#FF5F9E",
+    textLinkColor: "#E26EE5",
+    iconAfterLinkColor: "#ccc",
+    buttonOutlinedSecondaryBorder: "#E26EE5",
+    buttonOutlinedSecondaryBgHover: "#E26EE5",
+    buttonOutlinedSecondaryColorHover: "#fff",
+    badgeBg: "#FF3D00",
+    badgeText: "#fff",
+    white: "#f6f9fc",    
+    black:"#000"
 };
-export const avatar={
-    small:{
-        width:'50px',
-        height:'50px'
-    },
-    medium:{
-        width:'60px',
-        height: '60px'
-    }
-    ,
-
-    large:{
-        width:'60px',
-        height:'80px'
-    },
-    default:{
-        width:'70px',
-        height:'70px'
-    }
-
-}
-
-export const topography={
-    backgroundcolor:'rgb(255, 237, 237)',
-}
