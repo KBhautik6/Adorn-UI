cfc66b06f2a2dd64fc32a544ff63446c3d4f8a8e
ADRIXUS <kbhautik6>
2023-12-22 14:17:48 +0530

    change in styled.js file

--- src/stories/Button/Button.Styled.js
+++ src/stories/Button/Button.Styled.js
@@ -2,37 +2,39 @@ import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 
 export const StyledButtonComponent = styled.button`
-  text-align:center;
-  text-transform:uppercase;
-  font-size: 1.rem;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 1rem;
   font-weight: 600;
-  text-align:center;
-  padding:10px 20px;
-  border: 2px solid ;
-  border-color:${theme.white};
+  text-align: center;
+  padding: 10px 20px;
+  border: 2px solid;
+  border-color: ${theme.white};
 
-  background-color:${({colorinput})=>colorinput==="secondary"?theme.white:theme?.[colorinput]};
-  color:${({colorinput})=>colorinput==="secondary"?theme?.[colorinput]:theme.white};
+  background-color: ${({ colorinput }) =>
+    colorinput === "secondary" ? theme.white : theme?.[colorinput]};
+  color: ${({ colorinput }) =>
+    colorinput === "secondary" ? theme?.[colorinput] : theme.white};
 `;
 
-export const StyledOutlineButton=styled(StyledButtonComponent)`
-  border:2px solid ;
-  border-color:${({colorinput})=>theme?.[colorinput]};
-  color:${({colorinput})=>theme?.[colorinput]};
-  background-color:${({inputcolor})=>theme?.[inputcolor]};
-  
-  &:hover,
-  &:active{
-    background-color:${({colorinput})=>theme?.[colorinput]};
-    color:${theme.white}; 
-  }
-`
+export const StyledOutlineButton = styled(StyledButtonComponent)`
+  border: 2px solid;
+  border-color: ${({ colorinput }) => theme?.[colorinput]};
+  color: ${({ colorinput }) => theme?.[colorinput]};
+  background-color: ${({ inputcolor }) => theme?.[inputcolor]};
 
-export const StyledButtontext=styled(StyledButtonComponent)`
-  color:${({colorinput})=>theme?.[colorinput]};
+  &:hover,
+  &:active {
+    background-color: ${({ colorinput }) => theme?.[colorinput]};
+    color: ${theme.white};
+  }
+`;
+
+export const StyledButtontext = styled(StyledButtonComponent)`
+  color: ${({ colorinput }) => theme?.[colorinput]};
   bottom: 2px solid transparent;
-  background-color:transparent;
-`
+  background-color: transparent;
+`;
 
 export const StyledContainer = styled.div`
   display: flex;
