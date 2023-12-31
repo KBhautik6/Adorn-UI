e170761c55cc1817b20689ff5ccbc8cbc2904e44
Bhautik <bhautik.kapadiya@adrixus.com>
2023-12-31 17:38:56 +0530

    Intial commit

--- src/stories/Card/Card.js
+++ src/stories/Card/Card.js
@@ -1,13 +1,80 @@
 import React from 'react';
-import { StyledCard } from './Card.styled';
+import { StyledCardBox,StyledCardParagraph, StyledCardTitle, StyledOutlineButton, StyledBadgesText, StyledBadgesmallImage, StyledDissMissBadges, StyledBadgesMiddleText, StyledHorizontalCard, StyleContainer } from './Card.styled';
 
-const Card = ({ title, content }) => {
-  return (
-    <StyledCard>
-      <h2>{title}</h2>
-      <p>{content}</p>
-    </StyledCard>
-  );
+export const Card = ({ title, content, variant, src, Badgetext, checkouttext, outofstock }) => {
+  if (variant === "SimpleTextCard") {
+    return (
+      <StyleContainer>
+        <StyledCardBox>
+          <div>
+            <StyledCardTitle>{title}</StyledCardTitle>
+            <StyledCardParagraph>{content}</StyledCardParagraph>
+          </div>
+        </StyledCardBox>
+      </StyleContainer>
+    );
+  }
+
+  if (variant === "CardWithBadges") {
+    return (
+      <StyleContainer>
+        <StyledCardBox>
+          <StyledBadgesText>{Badgetext}</StyledBadgesText>
+          <StyledBadgesmallImage src={src} alt={alt} />
+          <div>
+            <StyledCardTitle>{title}</StyledCardTitle>
+            <StyledCardParagraph>{content}</StyledCardParagraph>
+          </div>
+        </StyledCardBox>
+      </StyleContainer>
+    );
+  }
+
+  if (variant === "CardsWithDissmiss") {
+    return (
+      <StyleContainer>
+        <StyledCardBox>
+          <StyledDissMissBadges>{Badgetext}</StyledDissMissBadges>
+          <StyledBadgesmallImage src={src} alt={alt} />
+          <div>
+            <StyledCardTitle>{title}</StyledCardTitle>
+            <StyledCardParagraph>{content}</StyledCardParagraph>
+            <StyledOutlineButton>{checkouttext}</StyledOutlineButton>
+          </div>
+        </StyledCardBox>
+      </StyleContainer>
+    );
+  }
+
+  if (variant === "CardsWithTextOverlaying") {
+    return (
+      <StyleContainer>
+        <StyledCardBox>
+          <StyledDissMissBadges>{Badgetext}</StyledDissMissBadges>
+          <StyledBadgesmallImage src={src} alt={alt} />
+          <StyledBadgesMiddleText>{outofstock}</StyledBadgesMiddleText>
+          <div>
+            <StyledCardTitle>{title}</StyledCardTitle>
+            <StyledCardParagraph>{content}</StyledCardParagraph>
+            <StyledOutlineButton>{checkouttext}</StyledOutlineButton>
+          </div>
+        </StyledCardBox>
+      </StyleContainer>
+    );
+  }
+
+  if (variant === "HorizontalCard") {
+    return (
+      <StyleContainer>
+        <StyledHorizontalCard>
+          <StyledBadgesmallImage src={src} alt={alt} />
+          <div>
+            <StyledCardTitle>{title}</StyledCardTitle>
+            <StyledCardParagraph>{content}</StyledCardParagraph>
+            <StyledOutlineButton>{checkouttext}</StyledOutlineButton>
+          </div>
+        </StyledHorizontalCard>
+      </StyleContainer>
+    );
+  }
 };
-
-export default Card;
--- src/stories/Card/Card.stories.js
+++ src/stories/Card/Card.stories.js
@@ -1,21 +1,53 @@
 // Card.stories.js
 import React from 'react';
 import Card from './Card';
-import {StyleContainer} from "./Card.styled"
+
 export default {
   title: 'Card',
   component: Card,
 };
 
 export const SimpleTextOnlyCard = () => (
-  <StyleContainer>
-    <Card
-    title="Eren Yeager"
-    content="
-    Eren Yeager was a former member of the Survey Corps. He was
-    the main protagonist of Attack on Titan. He lived in
-    Shiganshina District,
-  "
+  <Card variant="SimpleTextCard" title="Eren Yeager" content="Eren Yeager was a former member of the Survey Corps. He was the main protagonist of Attack on Titan. He lived in Shiganshina District,"/>
+);
+
+export const CardWithBadges=()=>(
+  <Card variant="CardWithBadges"
+   BadgeText="The Traning"
+    src="https://i.pravatar.cc/40" 
+    title="Survey Corps" 
+    content="In the year 847, Eren, Mikasa Ackerman, and Armin Arlert joined the 104th Training Corps. The three graduated with Eren ranking 5th best, and they joined the Survey Corps following the battle of Trost District"/>
+);
+
+export const CardWithDissMiss = () => (
+  <Card
+    BadgeText="X"
+    src="https://i.pravatar.cc/40"
+    variant="CardsWithDissMiss" 
+    title="Survey Corps story"
+    content="Rs. 150"
+    checkouttext="checkout"
   />
-  </StyleContainer>
+);
+
+export const CardsWithTextOverlaping=()=>(
+  <Card BadgeText="X"
+  src="https://i.pravatar.cc/40"
+  variant="CardsWithTextOverlaying"
+  title="Survey Corps Story"
+  content="Rs.250"
+  checkouttext="Out of Stock"
+  outofStock="Out of Stock"
+  />
+)
+
+export const HorizontalCard=()=>(
+ <Card
+  src="https://i.pravatar.cc/40"
+  variant="HorizontalCard"
+  title="Founding Titan"
+  content="Despite having the ability to control the Titans, no Founding Titan of royal blood after the Great Titan War expressed any desire to get rid of the Titans outside the Walls, even if the inheritor had expressed that desire previously"
+  checkouttext="Read More"
+ />
+
 );
